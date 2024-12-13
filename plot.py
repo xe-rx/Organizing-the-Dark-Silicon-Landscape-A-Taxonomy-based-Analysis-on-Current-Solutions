6d47a1afd333f48d5c02a59bceaf2143cee608ac
Marouan <145381165+xe-rx@users.noreply.github.com>
2024-12-13 16:52:42 +0100

    boilerplate code for taxonomy

---
+++ plot.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+x = np.linspace(-5, 5, 100)
+y = np.linspace(-5, 5, 100)
+X, Y = np.meshgrid(x, y)
+Z = np.sin(np.sqrt(X**2 + Y**2))
+
+ax.plot_surface(X, Y, Z, cmap='viridis')
+plt.savefig('taxonomy.png')
+plt.show()
